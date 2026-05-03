284b793ac6da49744116e0e95d30d0cfd936bc4e
Sebas_u28 <89940767+sebas-u28@users.noreply.github.com>
2026-05-03 16:01:28 -0500

    Solución Error 6

--- src/app.js
+++ src/app.js
@@ -2,21 +2,3 @@
 
 // Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
 const mysql = require("mysql");
-
-// 1. Hardcoded Secret (Un secreto expuesto)
-const API_KEY = "12345-ABCDE-67890-FGHIJ";
-
-// 2. Uso de una función criptográfica débil o peligrosa
-function cifrar(datos) {
-  // Eval es extremadamente peligroso y SonarCloud lo detectará
-  return eval(datos);
-}
-
-// 3. Un bug lógico
-function comparar(a, b) {
-  if ((a = b)) {
-    // Error: es una asignación (=) en lugar de comparación (== ======)
-    return true;
-  }
-  return false;
-}
