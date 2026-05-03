9a1ac838eb30d98d0d64b03ecf2dd7543e1bc034
Sebas_u28 <89940767+sebas-u28@users.noreply.github.com>
2026-05-03 17:01:14 -0500

    Commit Prueba 7

--- src/app.js
+++ src/app.js
@@ -2,14 +2,3 @@
 
 // Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
 const mysql = require("mysql");
-
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
-// Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
