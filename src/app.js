46c2aa0b89a2f374740a15aca7c6d18869cbdf45
Sebas_u28 <89940767+sebas-u28@users.noreply.github.com>
2026-05-03 15:44:46 -0500

    Commit Error 2

--- src/app.js
+++ src/app.js
@@ -2,23 +2,3 @@
 
 // Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_URL,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
-// Función bien escrita (Segura)
-function calcularDescuento(precio, porcentaje) {
-  if (precio <= 0 || porcentaje == 0) {
-    return 0;
-  }
-  return precio * (porcentaje / 100);
-}
-
-// Code Smell: Función definida pero nunca usada
-function () {
-  console.log("Nadie me llama");
-}
