a442ecf219819370380d513d3a68e987be6e8428
Sebas_u28 <89940767+sebas-u28@users.noreply.github.com>
2026-05-03 15:48:29 -0500

    Commit Error 3

--- src/app.js
+++ src/app.js
@@ -2,3 +2,20 @@
 
 // Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_URL,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+// Función bien escrita (Segura)
+ calcularprecio, porcentaje) {
+  if (precio <= 
+  
+
+// Code Smell: Función definida pero nunca usada
+ funcion) {
+  .log("Nadie me llama"
+
