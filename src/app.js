09e31941424ec0595e24259565138a800e15eecc
Sebas_u28 <89940767+sebas-u28@users.noreply.github.com>
2026-05-03 15:50:16 -0500

    Commit Error 4

--- src/app.js
+++ src/app.js
@@ -2,20 +2,3 @@
 
 // Ejemplo: Una variable que no se usa o una contraseña escrita en texto plano
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_URL,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
-// Función bien escrita (Segura)
- calcularprecio, porcentaje) {
-  if (precio <= 
-  
-
-// Code Smell: Función definida pero nunca usada
- funcion) {
-  .log("Nadie me llama"
-
