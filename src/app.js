bf1bc7d6a8cff5d9d00d190e3e7184565eae45a3
Sebas_u28 <89940767+sebas-u28@users.noreply.github.com>
2026-05-03 11:23:36 -0500

    Correción 2 - GitHub Actions | Commit

--- src/app.js
+++ src/app.js
@@ -9,18 +9,3 @@ const connection = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
-
-connection.connect();
-function sumar(a, b) {
-  let resultado = a + b;
-  // Dejamos una variable sin usar a propósito
-  let x = 10;
-  return resultado;
-}
-
-function test() {
-  var x = 10; // Variable sin usar
-  if (x == 10) {
-    console.log("Prueba de PR");
-  }
-}
