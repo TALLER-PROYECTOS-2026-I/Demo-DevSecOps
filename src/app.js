6d3d5d9f90f0c6dc85953c1cf99168171cb9b7d2
Sebas_u28 <89940767+sebas-u28@users.noreply.github.com>
2026-05-03 11:31:02 -0500

    Agregando vulnerabilidades para prueba de seguridad

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ const connection = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
+
+// 1. Hardcoded Secret (Un secreto expuesto)
+const API_KEY = "12345-ABCDE-67890-FGHIJ";
+
+// 2. Uso de una función criptográfica débil o peligrosa
+function cifrar(datos) {
+  // Eval es extremadamente peligroso y SonarCloud lo detectará
+  return eval(datos);
+}
+
+// 3. Un bug lógico
+function comparar(a, b) {
+  if ((a = b)) {
+    // Error: es una asignación (=) en lugar de comparación (==)
+    return true;
+  }
+  return false;
+}
